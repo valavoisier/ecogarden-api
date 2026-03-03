d4ee59d57cc0f900e5cb9412cc39155d68159ff4
Valerie Lavoisier <val.lavoisier3@gmail.com>
2026-03-03 17:10:37 +0100

    chore: initial commit - skeleton + Conseil entity

---
+++ src/Entity/Conseil.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ConseilRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ConseilRepository::class)]
+class Conseil
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $contenu = null;
+
+    #[ORM\Column]
+    private array $mois = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getContenu(): ?string
+    {
+        return $this->contenu;
+    }
+
+    public function setContenu(string $contenu): static
+    {
+        $this->contenu = $contenu;
+
+        return $this;
+    }
+
+    public function getMois(): array
+    {
+        return $this->mois;
+    }
+
+    public function setMois(array $mois): static
+    {
+        $this->mois = $mois;
+
+        return $this;
+    }
+}
