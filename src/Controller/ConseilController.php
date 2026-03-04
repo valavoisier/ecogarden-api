b41351bc89ffa7d997c3a511443de0651bdbf076
Valerie Lavoisier <val.lavoisier3@gmail.com>
2026-03-04 11:31:44 +0100

    feat: installation du serializer-pack et ajout de la route GET /api/conseils

--- src/Controller/ConseilController.php
+++ src/Controller/ConseilController.php
@@ -2,18 +2,17 @@
 
 namespace App\Controller;
 
+use App\Repository\ConseilRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
-
 final class ConseilController extends AbstractController
 {
-    #[Route('/conseil', name: 'app_conseil')]
-    public function index(): JsonResponse
+    #[Route('/api/conseils', name: 'conseil')]
+    public function getAllConseils(ConseilRepository $conseilRepository): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ConseilController.php',
-        ]);
+        $conseils = $conseilRepository->findAll();
+        //utilisation automatique du serializer pour convertir les objets en JSON
+        return $this->json($conseils);
     }
 }
