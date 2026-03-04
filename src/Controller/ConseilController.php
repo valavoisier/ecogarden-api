501a3c9945025776d17762515e28faba59173540
Valerie Lavoisier <val.lavoisier3@gmail.com>
2026-03-04 11:47:10 +0100

    documentation

--- src/Controller/ConseilController.php
+++ src/Controller/ConseilController.php
@@ -7,12 +7,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 final class ConseilController extends AbstractController
-{
-    #[Route('/api/conseils', name: 'conseil')]
+
+{/**
+  *Cette méthode permet de récupérer l'ensemble des conseils 
+ */
+    #[Route('/api/conseils', name: 'conseil', methods: ['GET'])]
     public function getAllConseils(ConseilRepository $conseilRepository): JsonResponse
     {
         $conseils = $conseilRepository->findAll();
-        //utilisation automatique du serializer pour convertir les objets en JSON
+        //utilisation automatique du serializer (installation du symfony/serializer-pack) pour convertir les objets en JSON
         return $this->json($conseils);
     }
 }
